42b696fdc2cf3b31f76e06f214cc01c4aa3ca458
TApplencourt <tapplencourt@gmail.com>
2016-04-07 19:31:51 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@
 from setuptools import setup, find_packages
 
 setup(name="irpy",
-      version=0.1,
+      version=0.2,
+      description = 'IRP for Python',
       py_modules=['irpy'],
       author='Thomas Applencourt',
 	  author_email='thomas.applencourt@irsamc.ups-tlse.fr',
 	  url='https://github.com/TApplencourt/IRPy',
+	  download_url = 'https://github.com/TApplencourt/IRPy/archive/master.tar.gz',
 )
