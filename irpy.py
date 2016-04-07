df6ab25fd824b4a975c2acd96f3a60c616b1388b
TApplencourt <tapplencourt@gmail.com>
2016-04-07 16:09:55 +0200

    Add doc

--- irpy.py
+++ irpy.py
@@ -4,9 +4,16 @@ import logging
 from collections import defaultdict
 from threading import Lock
 
+#Handle your execution stack
 D_PATH = defaultdict(lambda: [None])
-D_LOCK= defaultdict(lambda: defaultdict(lambda: Lock()))
+#Handle your lock stack
+D_LOCK = defaultdict(lambda: defaultdict(lambda: Lock()))
 
+
+#  _                                             _ 
+# |_ _  | |  _         _|_ |_   _    |  _   _. _|_ 
+# | (_) | | (_) \/\/    |_ | | (/_   | (/_ (_|  |  
+#
 def irp_ancestor(IRP_instance, EoI, visited=None):
     """
     Return the ancestor: A node reachable by repeated proceeding from child to parent.
@@ -29,58 +36,94 @@ def irp_ancestor(IRP_instance, EoI, visited=None):
     return visited
 
 
+# ___                                                  
+#  |     _  _. ._ / _|_    _   _ _|_   ._   _   _|  _  
+# _|_   (_ (_| | |   |_   (_| (/_ |_   | | (_) (_| (/_ 
+#                          _|
 def get_irp_node(IRP_instance, provider, private_node):
     """
-    Provider is a function used to compute the node.
-    Private_node, is private value of the node ("_node").
-    Parent is the node who want to acess to this particular node.
+    'provider' is a function used to compute the node.
+    'private_node', is private value of the node ("_node").
+    'parent' is the node who want to access to this particular node.
+    'IRP_instance' is an instance of the class who use IRPy.
 
-    This function set
+        First, this function return the 'private_node' value,
+               if it not existing yet, we will set it.
+
+        Secondly, We handle your own stack of function execution
+               in order to add into ${private_node}_parent all
+               the caller.
+
+    This function is (maybe) trade safe.
     """
     with D_LOCK[IRP_instance][private_node]:
 
         logging.debug("Ask for %s", private_node[1:])
-    
+
+        #Handle your execution stack
         caller_name = D_PATH[IRP_instance][-1]
-    
         D_PATH[IRP_instance].append(private_node)
-    
+
+        #Get and set the value node
         try:
             value = getattr(IRP_instance, private_node)
         except AttributeError:
             logging.debug("Provide")
-    
+
             value = provider(IRP_instance)
-    
+
             setattr(IRP_instance, private_node, value)
         else:
             logging.debug("Already provided")
-    
+        #Set the parent
         finally:
             #Handle the mutability
             local_parent = "{0}_parent".format(private_node)
             if caller_name:
-    
+
                 try:
                     setattr(IRP_instance, local_parent, set([caller_name]))
                 except AttributeError:
                     s = getattr(IRP_instance, local_parent)
                     setattr(IRP_instance, local_parent, set([caller_name]) | s)
-    
-        assert(D_PATH[IRP_instance].pop() == private_node)
-    
+
+        #Handle your execution stack
+        assert (D_PATH[IRP_instance].pop() == private_node)
+
     return value
 
-def set_irp_node(self, provider, private_node,value):
-    for i in irp_ancestor(self, private_node) - set([private_node]):
-        with D_LOCK[self][i]:
-            delattr(self, i)
 
-    with D_LOCK[self][private_node]:
-        setattr(self, private_node, value)
+def set_irp_node(IRP_instance, provider, private_node, value):
+    """
+    'provider' is a function used to compute the node.
+    'private_node', is private value of the node ("_node").
+    'parent' is the node who want to access to this particular node.
+    'value' is the value of the node who want to set.
 
+    First we remove all the private variable of the node who use this 
+        particular node. Then we set the new value
+
+    This function is (maybe) trade safe.  
+    """
+    for i in irp_ancestor(IRP_instance, private_node) - set([private_node]):
+        with D_LOCK[IRP_instance][i]:
+            delattr(IRP_instance, i)
+
+    with D_LOCK[IRP_instance][private_node]:
+        setattr(IRP_instance, private_node, value)
+
+
+#  _                              
+# | \  _   _  _  ._ _. _|_  _  ._ 
+# |_/ (/_ (_ (_) | (_|  |_ (_) |  
+#
 def irp_node(provider):
+    """
+    'provider' is a function.
 
+    This is the decorator. It is really similar to the 'property' function.
+    In fact we return a property with custom fget and fset
+    """
     str_provider = provider.__name__
     private_node = "_{0}".format(str_provider)
 
@@ -94,7 +137,12 @@ def irp_node(provider):
 
 
 def irp_node_mutable(provider):
+    """
+    'provider' is a function.
 
+    This is the decorator. It is really similar to the 'property' function.
+    In fact we return a property with custom fget and fset
+    """
     str_provider = provider.__name__
     private_node = "_{0}".format(str_provider)
 
@@ -102,33 +150,38 @@ def irp_node_mutable(provider):
         return get_irp_node(self, provider, private_node)
 
     def fset(self, value):
-        return set_irp_node(self, provider, private_node,value)
-
+        return set_irp_node(self, provider, private_node, value)
 
     return property(fget=fget, fset=fset)
 
 
-def irp_leafs_mutable(*tag_name):
+def irp_leafs_mutable(*irp_leaf):
+    "This a named decorator"
+    'For all the node in irp_leaf we create the property associated'
 
-    def tags_decorator(func):
-        def func_wrapper(self,*args, **kwargs):
+    'We set the new property and the we execute the function'
 
-            for str_provider in tag_name: 
+    def leaf_decorator(func):
+        def func_wrapper(self, *args, **kwargs):
+
+            for str_provider in irp_leaf:
                 private_node = "_{0}".format(str_provider)
-                
+
                 def provider(self):
                     return getattr(self, private_node)
 
                 def fget(self):
                     return get_irp_node(self, provider, private_node)
 
-                def fset(self,value):
-                    return set_irp_node(self, provider, private_node,value)
+                def fset(self, value):
+                    return set_irp_node(self, provider, private_node, value)
 
-                setattr(self.__class__,str_provider,property(fget=fget,fset=fset))
-                    
-            return func(self,*args, **kwargs)
-    
+                #If this ugly? Yeah... Is this an issue? I don't realy know
+                setattr(self.__class__, str_provider, property(fget=fget,
+                                                               fset=fset))
+
+            return func(self, *args, **kwargs)
+
         return func_wrapper
-    
-    return tags_decorator
+
+    return leaf_decorator
--- run_test.py
+++ run_test.py
@@ -4,6 +4,19 @@ from irpy import irp_node
 from irpy import irp_node_mutable
 from irpy import irp_leafs_mutable
 
+
+def init_loggin():
+    import logging
+
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+
+    str_ = '[%(relativeCreated)d ms] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'
+    formatter = logging.Formatter(str_)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
 class t_factory(object):
     """
     t(u(d1,d2),v(d3,d4),w(d5))
@@ -15,7 +28,7 @@ class t_factory(object):
     """
 
     @irp_leafs_mutable("d1")
-    def __init__(self,d1,d2,d3,d4,d5):
+    def __init__(self, d1, d2, d3, d4, d5):
         self.d1 = d1
         self.d2 = d2
         self.d3 = d3
@@ -28,38 +41,34 @@ class t_factory(object):
 
     @irp_node
     def u1(self):
-        return self.fu(self.d1,self.d2)
+        return self.fu(self.d1, self.d2)
 
     @irp_node
     def v(self):
-        return self.u2+self.w+2
+        return self.u2 + self.w + 2
 
     @irp_node
     def u2(self):
-        return self.fu(self.d3,self.d4)
+        return self.fu(self.d3, self.d4)
 
     @irp_node
     def w(self):
-        return self.d5+3
+        return self.d5 + 3
 
-    def fu(self,x,y):
-        return x+y+1
+    def fu(self, x, y):
+        return x + y + 1
 
 
 if __name__ == '__main__':
 
-    import logging
-    
-    logger = logging.getLogger()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('[%(relativeCreated)d ms] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    init_loggin()
 
-    F = t_factory(1,5,8,10,7)
-    assert(F.t == 42)
+    F = t_factory(1, 5, 8, 10, 7)
+    #Show the dynamic resolution of node
+    assert (F.t == 42)
+    #Show the lazy evaluation
+    assert (F.t == 42)
 
+    #Show the coherence and mutability of tree
     F.d1 = 2
-    assert(F.t == 43)
-    assert(F.t == 43)
+    assert (F.t == 43)
