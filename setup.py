770ecaa62540f1a310cd178843b8557912178cee
TApplencourt <tapplencourt@gmail.com>
2016-04-07 11:41:13 +0200

    Change yaml and setup

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ from setuptools import setup, find_packages
 setup(name="irpy",
       version=0.1,
       py_modules=['irpy'],
+      author='Thomas Applencourt',
+	  author_email='thomas.applencourt@irsamc.ups-tlse.fr',
+	  url='https://github.com/TApplencourt/IRPy',
 )
