0d19ffb567c0d3e5b8d1246201c74fe0b3e69147
TApplencourt <tapplencourt@gmail.com>
2016-04-11 16:17:56 +0200

    Refractoring

--- irpy.py
+++ irpy.py
@@ -6,18 +6,19 @@ from threading import Lock
 
 #Handle your execution stack
 D_PATH = defaultdict(lambda: [None])
+#Handle the instance variable
 D_INIT = defaultdict(lambda: defaultdict(lambda: False))
 #Handle your lock stack
 D_LOCK = defaultdict(lambda: defaultdict(lambda: Lock()))
 
-#  _                                             _ 
-# |_ _  | |  _         _|_ |_   _    |  _   _. _|_ 
-# | (_) | | (_) \/\/    |_ | | (/_   | (/_ (_|  |  
-#
-
-
+#  _                                            
+# |_ _  | |  _         _|_ |_   _     _  _. ._  
+# | (_) | | (_) \/\/    |_ | | (/_   _> (_| |_) 
+#                                           |   
 def irp_sap(IRP_instance, node, direction, visited=None):
-
+    """
+    Direction is $parents or $children, recurse folowing this
+    """
     if visited is None:
         visited = set()
 
@@ -35,121 +36,118 @@ def irp_ancestor(IRP_instance, node):
     """
     Return the ancestor: A node reachable by repeated proceeding from child to parent.
     The parent of a node is stored in _$node_parent. 
-    If the node is a Root, no parent are present.
     """
-    return irp_sap(IRP_instance, node, "parent") - set([node])
+    return irp_sap(IRP_instance, node, "parents") - set([node])
 
 
 def irp_descendant(IRP_instance, node):
     """
-    Return the ancestor: A node reachable by repeated proceeding from child to parent.
-    The parent of a node is stored in _$node_parent. 
-    If the node is a Root, no parent are present.
+    Return the descendant: A node reachable by repeated proceeding from parent to child.
+    The parent of a node is stored in _$node_child. 
     """
-    return irp_sap(IRP_instance, node, "child") - set([node])
+    return irp_sap(IRP_instance, node, "children") - set([node])
 
 
 # ___                                                  
 #  |     _  _. ._ / _|_    _   _ _|_   ._   _   _|  _  
 # _|_   (_ (_| | |   |_   (_| (/_ |_   | | (_) (_| (/_ 
 #                          _|
-def get_irp_node(IRP_instance, provider, private_node):
+#Satisfaction
+def get_irp_node(IRP_instance, provider, pri_node):
     """
     'provider' is a function used to compute the node.
-    'private_node', is private value of the node ("_node").
-    'parent' is the node who want to access to this particular node.
+    'pri_node', is pri value of the node ("_node").
     'IRP_instance' is an instance of the class who use IRPy.
 
-        First, this function return the 'private_node' value,
+        First, this function return the 'pri_node' value,
                if it not existing yet, we will set it.
 
         Secondly, We handle your own stack of function execution
-               in order to add into ${private_node}_parent all
+               in order to add into ${pri_node}_parent all
                the caller.
 
     This function is (maybe) trade safe.
     """
-    with D_LOCK[IRP_instance][private_node]:
+    with D_LOCK[IRP_instance][pri_node]:
 
-        logging.debug("Ask for %s", private_node)
+        logging.debug("Ask for %s", pri_node)
 
-        #Handle your execution stack
+        #Handle the  execution stack
         caller_name = D_PATH[IRP_instance][-1]
 
-        D_PATH[IRP_instance].append(private_node)
+        D_PATH[IRP_instance].append(pri_node)
 
         #Get and set the value node
 
-        coh = "{0}_coherent".format(private_node)
+        coh = "{0}_coherent".format(pri_node)
 
         if not getattr(IRP_instance, coh):
-            raise AttributeError, "Node is incoherent {0}".format(private_node)
+            raise AttributeError, "Node is incoherent {0}".format(pri_node)
 
         try:
-            value = getattr(IRP_instance, private_node)
+            value = getattr(IRP_instance, pri_node)
         except AttributeError:
             logging.debug("Provide")
 
             value = provider(IRP_instance)
-            setattr(IRP_instance, private_node, value)
+            setattr(IRP_instance, pri_node, value)
         else:
             logging.debug("Already provided")
-        finally:
-            #Handle the mutability
-            if caller_name:
-                local_parent = "{0}_parent".format(private_node)
 
-                s = getattr(IRP_instance, local_parent)
-                setattr(IRP_instance, local_parent, set([caller_name]) | s)
+        #Handle the mutability
+        if caller_name:
 
-                local_child = "{0}_child".format(caller_name)
-                s = getattr(IRP_instance, local_child)
-                setattr(IRP_instance, local_child, set([private_node]) | s)
+            #Set parent
+            local_parent = "{0}_parents".format(pri_node)
+            s = getattr(IRP_instance, local_parent)
+            setattr(IRP_instance, local_parent, set([caller_name]) | s)
+
+            #Set children
+            local_child = "{0}_children".format(caller_name)
+            s = getattr(IRP_instance, local_child)
+            setattr(IRP_instance, local_child, set([pri_node]) | s)
 
         #Handle your execution stack
-        assert (D_PATH[IRP_instance].pop() == private_node)
+        assert (D_PATH[IRP_instance].pop() == pri_node)
 
     return value
 
 
-def set_irp_node(IRP_instance, provider, private_node, value):
+def set_irp_node(IRP_instance, provider, pri_node, value):
     """
     'provider' is a function used to compute the node.
-    'private_node', is private value of the node ("_node").
+    'pri_node', is pri value of the node ("_node").
     'parent' is the node who want to access to this particular node.
     'value' is the value of the node who want to set.
 
-    First we remove all the private variable of the node who use this 
+    First we remove all the pri variable of the node who use this 
         particular node. Then we set the new value
 
     This function is (maybe) trade safe.  
     """
 
-    logging.debug("Set node %s", private_node)
+    logging.debug("Set node %s", pri_node)
+    with D_LOCK[IRP_instance][pri_node]:
+        setattr(IRP_instance, pri_node, value)
 
-    l_ancestor = irp_ancestor(IRP_instance, private_node)
-    l_descendant = irp_descendant(IRP_instance, private_node)
+    l_ancestor = irp_ancestor(IRP_instance, pri_node)
+    l_descendant = irp_descendant(IRP_instance, pri_node)
 
-    logging.debug("Unset parent: %s", l_ancestor)
-
-    #Remove the memo and tell is good to compute
+    logging.debug("Unset parents: %s", l_ancestor)
     for i in l_ancestor:
 
         with D_LOCK[IRP_instance][i]:
             delattr(IRP_instance, i)
 
-    for i in l_ancestor | set([private_node]):
-        setattr(IRP_instance, "{0}_coherent".format(i), True)
+    logging.debug("Set ancestor coherence to True: %s", l_ancestor)
+    for i in l_ancestor | set([pri_node]):
+        with D_LOCK[IRP_instance][i]:
+            setattr(IRP_instance, "{0}_coherent".format(i), True)
 
-    logging.debug("Unset child: %s", l_descendant)
-
-    #Remove unset the tree
+    logging.debug("Set descendant coherence to False: %s", l_descendant)
     for i in l_descendant:
         with D_LOCK[IRP_instance][i]:
             setattr(IRP_instance, "{0}_coherent".format(i), False)
-
-    with D_LOCK[IRP_instance][private_node]:
-        setattr(IRP_instance, private_node, value)
 
 
 #  _                              
@@ -157,54 +155,54 @@ def set_irp_node(IRP_instance, provider, private_node, value):
 # |_/ (/_ (_ (_) | (_|  |_ (_) |  
 #
 class property_irp(object):
-    def __init__(self, provider=None, str_provider=None, immutability=True):
+    """
+    My little Property
+    My little Property
+    My little Property...  friend
 
-        if provider:
-            self.provider = provider
-            self.str_provider = provider.__name__
-            self.private_node = "_{0}".format(self.str_provider)
+    Provider: If a function who will be used to compute the node
+    Str_provider: If the name of the node
+    Immutability: If immuntabilty is set you cannot set the node
+    """
 
-        elif str_provider:
-            self.str_provider = str_provider
-            self.private_node = "_{0}".format(self.str_provider)
+    def __init__(self, provider, str_provider=None, immutability=True):
 
-            def provider(self):
-                return getattr(self, self.private_node)
+        self.provider = provider
+        self.str_provider = str_provider if str_provider else provider.__name__
 
-            self.provider = provider
-
+        self.pri_node = "_{0}".format(self.str_provider)
         self.immutability = immutability
 
     def set_obj_attr(self, obj):
 
+        #For each instance of the provider initiante somme ussfull values
         if not D_INIT[self.str_provider][obj]:
 
             d = {
-                "_{0}_child": set(),
-                "_{0}_parent": set(),
+                "_{0}_children": set(),
+                "_{0}_parents": set(),
                 "_{0}_coherent": True
             }
 
             for attr, value in d.items():
                 setattr(obj, attr.format(self.str_provider), value)
 
-            self.init = True
-
             D_INIT[self.str_provider][obj] = True
 
     def __get__(self, obj, objtype):
-
+        "Get the value of the node"
         self.set_obj_attr(obj)
-        return get_irp_node(obj, self.provider, self.private_node)
+
+        return get_irp_node(obj, self.provider, self.pri_node)
 
     def __set__(self, obj, value):
+        "Set the value of the node"
         self.set_obj_attr(obj)
 
         if self.immutability:
-            raise AttributeError, "Immutable Node {0}".format(
-                self.private_node)
+            raise AttributeError, "Immutable Node {0}".format(self.pri_node)
         else:
-            set_irp_node(obj, self.provider, self.private_node, value)
+            set_irp_node(obj, self.provider, self.pri_node, value)
 
 
 def property_irp_mutable(provider):
@@ -228,11 +226,14 @@ def property_irp_leaves_mutables(*irp_leaf):
 
             for str_provider in irp_leaf:
 
-                #If this ugly? Yeah... Is this an issue? I don't realy know
+                def provider(self):
+                    return getattr(self, self.pri_node)
 
-                setattr(self.__class__, str_provider,
-                        property_irp(str_provider=str_provider,
-                                     immutability=False))
+                p = property_irp(provider=provider,
+                                 str_provider=str_provider,
+                                 immutability=False)
+                #If this ugly? Yeah... Is this an issue? I don't realy know
+                setattr(self.__class__, str_provider, p)
 
             return func(self, *args, **kwargs)
 
