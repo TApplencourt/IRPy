86ed50effc09115b4a75fff6aee12316c6846264
TApplencourt <tapplencourt@gmail.com>
2016-04-13 11:05:03 +0200

    Add license

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@
 @author: T. Applencourt
 '''
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name="irpy",
       version=0.3,
       description = 'IRP for Python',
+      long_description=read('README.md'),
       py_modules=['irpy'],
       author='Thomas Applencourt',
 	  author_email='thomas.applencourt@irsamc.ups-tlse.fr',
 	  url='https://github.com/TApplencourt/IRPy',
 	  download_url = 'https://github.com/TApplencourt/IRPy/archive/master.tar.gz',
+	  licence='WTFPL',
 )
