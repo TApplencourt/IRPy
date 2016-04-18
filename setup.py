53b67dbee84dc0ad91c83a3edeec56803d5a5d0a
TApplencourt <tapplencourt@gmail.com>
2016-04-18 13:24:21 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name="irpy",
-      version="0.3.1",
+      version="0.4",
       description = 'IRP for Python',
       long_description=read('README.md'),
-      py_modules=['irpy'],
+      packages=['irpy'],
       author='Thomas Applencourt',
-	  author_email='thomas.applencourt@irsamc.ups-tlse.fr',
-	  url='https://github.com/TApplencourt/IRPy',
-	  download_url = 'https://github.com/TApplencourt/IRPy/archive/master.tar.gz',
-	  licence='WTFPL',
+	    author_email='thomas.applencourt@irsamc.ups-tlse.fr',
+	    url='https://github.com/TApplencourt/IRPy',
+	    download_url = 'https://github.com/TApplencourt/IRPy/archive/master.tar.gz',
+	    license='WTFPL',
 )
