362ddbde7bb40c744b211636f51ecdd5612ef1c7
TApplencourt <tapplencourt@gmail.com>
2016-04-27 14:21:10 +0200

    Cython is now maybe a optional dependancy

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup
-from Cython.Build import cythonize
+from distutils.core import setup
+from distutils.extension import Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    USE_CYTHON = False
+else:
+    USE_CYTHON = True
+
+ext = '.pyx' if USE_CYTHON else '.c'
+extensions = [Extension("example", ["example"+ext])]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
 
 setup(
     name="irpy",
     version="0.5.0",
-    ext_modules = cythonize("irpy.pyx"),
+    ext_modules = extensions,
     description = 'IRP for Python',
     long_description='Look a github README please.',
     url='https://github.com/TApplencourt/IRPy',
